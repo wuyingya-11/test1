e2b68bafada944577d55ffae66f005623963a1a7
wuyingya <2326860098@qq.com>
2021-11-01 16:56:50 +0800

    Auto stash before merge of "main" and "origin/main"

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 int main()
 {
+<<<<<<< Updated upstream
     cout << "heee11 hello wuyingya" << endl;
+=======
+    cout << "hee hello wuyingya" << endl;
+>>>>>>> Stashed changes
 }
